8148377f6f38ef25c8a8d1c6f60f80d6f6845ca5
천운령 <cur@plani.co.kr>
2024-01-31 08:50:33 +0900

    first commit

---
+++ src/main/java/org/example/OptionCombinationApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionCombinationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptionCombinationApplication.class, args);
+    }
+
+}
